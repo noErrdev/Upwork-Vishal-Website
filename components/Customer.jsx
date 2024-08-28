28dceaaada47ba081f3f90339984d18f9343e96e
noErrdev <dbongo0701@gmail.com>
2024-08-29 02:59:29 +0900

    test

--- components/Customer.jsx
+++ components/Customer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 const Customer = () => {
   return (
-    <section className="bg-white px-4 sm:px-8 md:px-24 lg:px-48">
+    <section className="bg-white py-8 px-4 sm:px-8 md:px-24 lg:px-48">
       <div className="max-container">
         <div className="flex flex-col justify-between gap-4 xl:flex-row xl:m-0 items-center">
           <div className="w-full">
@@ -9,7 +9,7 @@ const Customer = () => {
           <div className="regular_2 text-black text-xl">
           Vishal is a seasoned Professional Engineer with over two decades of expertise in the oil & gas and petrochemical sectors. His impressive career includes leadership roles at industry giants such as ExxonMobil, Wood, Foster Wheeler, and Richard EPC.
           Vishal has excelled in contracting and plant operations, bringing deep knowledge and hands-on experience to every project life cycle phase. His technical understanding spans diverse cutting-edge technologies, including Refining, Gasification, Syngas, Gas to Liquid, Olefins Ethylene Cracking, and Polyethylene.
-          <br/>Driven by a passion for engineering excellence, Vishal founded MES to create a high-value center of excellence.</div>
+          </div>
           </div>
           <div className="lg:mt-0 lg:ml-8 w-full">
             <img 
@@ -21,7 +21,6 @@ const Customer = () => {
               />
           </div>
         </div>
-        <div className="mt-4">Under his visionary leadership, MES delivers seamless &ldquo;concept to commissioning&rdquo; process engineering solutions backed by a multidisciplinary team committed to innovation, development, and the advancement of the engineering profession. Vishal is dedicated to cultivating a company culture where creativity, technical brilliance, and a relentless pursuit of excellence thrive.</div>
       </div>
     </section>
   );
