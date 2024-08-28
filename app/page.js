da697c53c22bf0f737c622cba1eea374ac638bac
noErrdev <dbongo0701@gmail.com>
2024-08-29 04:19:13 +0900

    test

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
       <Services />
       <Projects />
       <Footer />
-      <div className="absolute -bottom-32 -right-24 z-20 hidden lg:block">
+      {/* <div className="absolute -bottom-32 -right-24 z-20 hidden lg:block">
         <svg width="240" height="240" viewBox="0 0 16 16" className="mr-0">
           <path d="M8 1 Q7 0 6 1 L1 6 Q0 7 1 8 L6 13 Q7 14 8 13 L13 8 Q14 7 13 6 L8 1 Z" fill="#224194"/>
         </svg>
@@ -34,7 +34,7 @@ export default function Home() {
         <svg width="120" height="120" viewBox="0 0 16 16" className="mr-0">
           <path d="M8 1 Q7 0 6 1 L1 6 Q0 7 1 8 L6 13 Q7 14 8 13 L13 8 Q14 7 13 6 L8 1 Z" fill="#88c64b" />
         </svg>
-      </div>
+      </div> */}
     </div>
   );
 }
